0505f062cbfd83b9e4d732dea73a7dc69bf15a40
AreebSiddiqui <areeb40@hotmail.com>
2019-08-16 19:02:31 +0500

    v1

---
+++ listing2.7/index.js
@@ -0,0 +1,6 @@
+const mustache = require ("mustache");
+let result  = mustache.render("Hi,{{first_name}} {{second_name}}!",{
+    first_name:"Areeb",
+    second_name:"Siddiqui"
+    });
+    console.log(result);
